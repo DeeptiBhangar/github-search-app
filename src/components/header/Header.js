7ef1298c02d30a612fd44304e31904fbb62117c9
DeeptiBhangar <guptadeepti626@gmail.com>
2020-12-14 00:07:30 -0500

    Add header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1 className="app-header__title">GitHub Search</h1>
+    </header>
+  );
+};
+
+export default Header;
